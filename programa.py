35212ad2400a01ae9b55302d2d22fbefccfb45b1
APaiva1999 <37874420+APaiva1999@users.noreply.github.com>
2018-10-03 21:51:02 -0300

    Add files via upload

--- programa.py
+++ programa.py
@@ -1,11 +1,11 @@
 import json
 
 running = True
-comanda = {}
+comandas = {}
 with open('cardapio.json') as json_data:
     cardapio = json.load(json_data)
 while running:
-    menu = (input("Comanda Eletrônica\n0. Sair\n1. Imprimir cardápio\n2. Adicionar item\n3. Remover item\n4. Imprimir comanda\n-->"))
+    menu = input("Comanda Eletrônica\n0. Sair\n1. Imprimir cardápio\n2. Adicionar item\n3. Remover item\n4. Imprimir comanda\n-->")
     if menu!='1' and menu!='2' and menu!='3' and menu!='4' and menu!='0':
         print("Opção Inválida!")
     if menu =='0':
@@ -32,31 +32,33 @@ while running:
                 cardapio[pedido] = preco
                 print("Item adicionado")
         if escolha=="comanda":       
-            
+            comanda=input("Qual a mesa? ")
+            comandas[comanda]={}
             pedido = input("Faça sua escolha: ")
             if pedido in cardapio:
                 quantidade= int(input("Quantidade: "))
                 if pedido in comanda:
-                    comanda[pedido] +=quantidade
+                    comandas[comanda][pedido] +=quantidade
                 else:
-                    comanda[pedido] = quantidade
-                print("Quantidade atual de {0}: {1}".format(pedido,comanda[pedido]))
+                    comandas[comanda][pedido] = quantidade
+                print("Quantidade atual de {0}: {1}".format(pedido,comandas[comanda][pedido]))
             else:
                 print("Não há este produto no cardápio.")
     if menu =='3':
         escolha=(input("Deseja remover do cardapio ou da comanda?"))
         if escolha=='comanda':
+            comanda=input("De qual comanda deseja remover?")
             pedido=input("Pedido a ser removido:")
             if pedido in comanda:
                 quantidade=int(input("Quantidade a remover: "))
                 if quantidade<0:
                     print("Não é possível remover quantidade não positiva")
                 else:
-                    if (comanda[pedido]-quantidade)>=0:
-                        comanda[pedido]=comanda[pedido]-quantidade
-                        print ("Quantidade atual de {0} : {1}".format(pedido,comanda[pedido]))
-                    if comanda[pedido]== 0:
-                        del comanda[pedido]
+                    if (comandas[comanda][pedido]-quantidade)>=0:
+                        comandas[comanda][pedido]=comandas[comanda][pedido]-quantidade
+                        print ("Quantidade atual de {0} : {1}".format(pedido,comandas[comanda][pedido]))
+                    if comandas[comanda][pedido]== 0:
+                        del comandas[comanda][pedido]
                         
             else:
                 print("Este item não está na comanda")
@@ -64,15 +66,13 @@ while running:
             pedido=input('Qual item deseja remover do cardápio?')
             if pedido in cardapio:
                 del cardapio[pedido]
-            if pedido in comanda:
-                del comanda[pedido]
             else:
                 print("Item não cadastrado")
     if menu=='4':
         total=0
         for item in comanda:
-            total += cardapio[item] * comanda[item]
-            unidade= cardapio[item] * comanda[item]
+            total += cardapio[item] * comandas[comanda][item]
+            unidade= cardapio[item] * comandas[comanda][item]
             print("{0}:{1:.2f}R$".format(item,cardapio[item]))
             print("{0:.2f}R$".format(unidade))
         print("Valor total:{0:.2f}R$".format(total))
