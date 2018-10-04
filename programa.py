f0d3f7b273e982830397b1b1a056eb35210ec353
APaiva1999 <37874420+APaiva1999@users.noreply.github.com>
2018-10-03 21:33:55 -0300

    bugfix
    bug na hora que selecionava as opcoes, nao fosse numero inteiro, o programa travava

--- programa.py
+++ programa.py
@@ -6,12 +6,14 @@ with open('cardapio.json') as json_data:
     cardapio = json.load(json_data)
 while running:
     menu = int(input("Comanda Eletrônica\n0. Sair\n1. Imprimir cardápio\n2. Adicionar item\n3. Remover item\n4. Imprimir comanda\n-->"))
-    if menu ==0:
+    if menu!='1' and menu!='2' and menu!='3' and menu!='4' and menu!='0':
+        print("Opção Inválida!")
+    if menu =='0':
         print("Até mais")
         with open('cardapio.json','w') as f:
             f.write(json.dumps(cardapio))
         running = False
-    if menu ==1:
+    if menu =='1':
         if cardapio == {}:
             print("Não há nada aqui")
         else:
@@ -19,7 +21,7 @@ while running:
         for a,b in cardapio.items():
             print("- {0}({1:.2f}R$)".format(a,b))
     
-    if menu ==2:
+    if menu =='2':
         escolha= input("Gostaria de adicionar ao cardápio ou à comanda? " )
         if escolha=="cardápio" or escolha=="cardapio":
             pedido=input("O que deseja adicionar ao cardápio? " )
@@ -41,7 +43,7 @@ while running:
                 print("Quantidade atual de {0}: {1}".format(pedido,comanda[pedido]))
             else:
                 print("Não há este produto no cardápio.")
-    if menu ==3:
+    if menu =='3':
         escolha=(input("Deseja remover do cardapio ou da comanda?"))
         if escolha=='comanda':
             pedido=input("Pedido a ser removido:")
@@ -64,7 +66,7 @@ while running:
                 del cardapio[pedido]
             else:
                 print("Item não cadastrado")
-    if menu==4:
+    if menu=='4':
         total=0
         for item in comanda:
             total += cardapio[item] * comanda[item]
@@ -73,4 +75,3 @@ while running:
             print("{0:.2f}R$".format(unidade))
         print("Valor total:{0:.2f}R$".format(total))
         print("Valor total com serviços (10%):{0:.2f}R$".format(total*1.1))
-        
